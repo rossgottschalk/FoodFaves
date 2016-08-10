3f9c5d034989587a91bda6d2f0ecbc9ab64c1f91
Ross Gottschalk <rossgottschalk@yahoo.com>
2016-08-10 15:05:46 -0400

    Initial Commit

---
+++ FoodFaves/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FoodFaves
+//
+//  Created by Ross Gottschalk on 8/10/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
